3398e5e235c9b672a6839958d49aa35890d20212
ibrahima <ibrahimahotia.diallo@gmail.com>
2020-03-18 15:01:26 +0100

    Adding batch

---
+++ batch/src/main/java/com/ib/batch/MailConstants.java
@@ -0,0 +1,10 @@
+package com.ib.batch;
+
+public class MailConstants {
+
+    public static final String MY_EMAIL = "ibrahimahotia.diallo@gmail.com";
+
+    public static final String MY_PASSWORD = "5Nyedoiq8s";
+
+    public static final String FRIEND_EMAIL = "ibrahimahotia.diallo@gmail.com";
+}
